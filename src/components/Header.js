5df320350f78b06508c0964e35fa45c944909bec
GurkiratSidhu <g3sidhu@gmail.com>
2021-05-23 18:28:57 +0530

    commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,26 @@
 import React from 'react'
+import PropTypes from 'prop-types'
+import Button from './Button'
 
 const Header = ({title}) => {
+    const onClick = () => {
+        console.log('Click')
+    }
     return (
-        <header>
-            {title}
+        <header className='header'>
+           <h1> {title} </h1>
+           <Button color='grey' text='Hello' onClick= {onClick} />
         </header>
     )
 }
 
+Header.defaultProps = {
+    title: 'Task Tracker' ,
+}
+
+Header.propTypes = {
+    title: PropTypes.string,
+}
 
 
 export default Header
