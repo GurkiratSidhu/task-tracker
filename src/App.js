b52d493151fc8cd5bb68c540e3b58bfedab9e5da
GurkiratSidhu <g3sidhu@gmail.com>
2021-06-01 16:22:25 +0530

    dropdown toggle

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Header from './components/Header'
 import Tasks from './components/Tasks'
+import AddTask from './components/AddTask'
 import {useState} from 'react'
 
 
 function App() {
 
+  const [showAddTask,setShowAddTask] = useState(false)
 
   const [tasks, setTasks] = useState([
     {
@@ -28,6 +30,14 @@ function App() {
     },
 ])
 
+
+// Add Task
+const addTask = (task) => {
+  const id = Math.floor(Math.random()*10000) + 1
+  const newTask = {id, ...task}
+  setTasks([...tasks, newTask])
+}
+
 //delete task
 const deleteTask = (id) => {
 setTasks(tasks.filter((task) => task.id !== id)) 
@@ -43,7 +53,8 @@ const toggleReminder = (id) =>{
 
   return (
     <div className='container'>
-      <Header />
+      <Header onAdd={() => setShowAddTask(!showAddTask)} showAdd={showAddTask} />
+      {showAddTask && <AddTask onAdd={addTask} />}
   {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} /> : 'No Tasks to display' }
   
     </div>
