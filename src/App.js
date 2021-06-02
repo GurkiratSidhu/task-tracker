83712c3536a67a497c4a28b43a5169c3773364e0
GurkiratSidhu <g3sidhu@gmail.com>
2021-06-02 16:24:41 +0530

    added React Router for About page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import './App.css';
+import {BrowserRouter as Router, Route} from 'react-router-dom'
 import Header from './components/Header'
 import Tasks from './components/Tasks'
 import AddTask from './components/AddTask'
+import Footer from './components/Footer'
+import About from './components/About'
+
+
 import {useState} from 'react'
 
 
@@ -52,12 +57,24 @@ const toggleReminder = (id) =>{
 
 
   return (
+    <Router>
     <div className='container'>
       <Header onAdd={() => setShowAddTask(!showAddTask)} showAdd={showAddTask} />
-      {showAddTask && <AddTask onAdd={addTask} />}
+     
+  <Route path='/' exact render={(props)=>(
+  <>
+     {showAddTask && <AddTask onAdd={addTask} />}
   {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} /> : 'No Tasks to display' }
   
+  </>
+  )
+
+  } />
+
+  <Route path='/about' component={About} />
+  <Footer />
     </div>
+    </Router>
   );
 }
 
